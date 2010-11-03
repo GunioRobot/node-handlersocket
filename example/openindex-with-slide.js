1a3f4e6a864d60e6b2bc40298bb956dd35165292
koichik <koichik@improvement.jp>
2010-11-04 01:47:36 +0900

    added example with slide

---
+++ example/openindex-with-slide.js
@@ -0,0 +1,21 @@
+var hs = require('../lib/node-handlersocket'), asyncMap = require('slide/async-map');
+
+var indexDefs = [
+  ['test', 'EMPLOYEE', 'PRIMARY', [ 'EMPLOYEE_ID', 'EMPLOYEE_NO', 'EMPLOYEE_NAME' ]],
+  ['test', 'DEPARTMENT', 'PRIMARY', ['DEPARTMENT_ID', 'DEPARTMENT_NO', 'DEPARTMENT_NAME']],
+  ['test', 'ADDRESS', 'PRIMARY', ['ADDRESS_ID', 'STREET']]
+];
+
+var con = hs.connect();
+con.on('connect', function() {
+  asyncMap(indexDefs, function(indexDef, callback) {
+    con.openIndex.apply(con, indexDef.concat(callback))
+  }, function(err, indices) {
+    indices.forEach(function(index) {
+      console.log(index);
+    });
+    con.end();
+  });
+});
+
+

88892489b9182176a466eb5d62387f34c37ce1a5
koichik <koichik@improvement.jp>
2010-11-04 14:44:14 +0900

    added examples

---
+++ example/update.js
@@ -0,0 +1,12 @@
+var hs = require('../lib/node-handlersocket');
+
+var con = hs.connect({port : 9999});
+con.on('connect', function() {
+  con.openIndex('test', 'EMPLOYEE', 'PRIMARY', [ 'EMPLOYEE_ID', 'EMPLOYEE_NO',
+    'EMPLOYEE_NAME' ], function(err, index) {
+    index.update('=', 100, [100, 9999, 'EBIYURI'], function(err, rows) {
+      console.log(rows);
+      con.end();
+    });
+  });
+});
